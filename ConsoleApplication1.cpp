a6988c174df1f05acea5c77b2850e960e3d775dd
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2022-11-28 11:35:17 +0300

    Сложносоставные условия

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -3,10 +3,25 @@ int main()
 {	
 	int a, b;
 
-	printf("input a");
-	scanf_s("%i", &a);
-	printf("input b");
-	scanf_s("%i", &b);
+	printf("input pin1");
+	scanf_s("%i", &pin1);
+	printf("input pin2");
+	scanf_s("%i", &pin2);
+
+	if ((pin1 == 123) && (pin2 == 321)) {
+		printf("Добро пожаловать user");
+	}
+	else {
+		printf("Пользователь с таким логином или паролем не найден");
+	}
+
+	// Задача 2
+	long int login, password;
+
+	printf("input login");
+	scanf_s("%ld", &login);
+	printf("input password");
+	scanf_s("%ld", &password);
 
 	if (a > b) {
 		printf("A is bigger");
