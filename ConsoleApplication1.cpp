f530d5ce7ec8e06baf87971617e86e1edb54cb9c
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2022-12-05 20:21:51 +0300

    Восточный календарь

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,52 +1,84 @@
 ﻿#include <stdio.h>
-int main() 
-{
-	int day;
+#include <math.h>
 
-	printf("Input day number");
+int main() {
+
+	int day, month, year;
+
+	printf("input day");
 	scanf_s("%i", &day);
+	printf("input month");
+	scanf_s("%i", &month);
+	printf("input year");
+	scanf_s("%i", &year);
 
-	switch (day)
-	{
-		case 1:
-		{
-			printf("Monday");
-			break;
-		}
-		case 2: 
-		{
-			printf("Tuesday");
-			break;
-		}
-		case 3:
-		{
-			printf("Wednesday");
-			break;
-		}
-		case 4:
-		{
-			printf("Thursday");
-			break;
-		}
-		case 5:
-		{
-			printf("Friday");
-			break;
-		}
-		case 6:
-		{
-			printf("Saturday");
-			break;
-		}
-		case 7:
-		{
-			printf("Sunday");
-			break;
-		}
-		default: 
-		{
-			printf("Input error");
-			break;
-		}		
+	printf("%i %i %i ", day, month, year);
+
+	//Високосный год
+
+	if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
+		printf("Leap year ");
 	}
+	else {
+		printf("Not leap year ");
+	}
+
+	// Китайский гороскоп
+
+	int eastern = (year - 1936) % 12;
+
+	switch (eastern) {
+		case 0: {
+			printf("Year of the rat");
+			break;
+		}
+		case 1: {
+			printf("Year of the bull");
+			break;
+		}
+		case 2: {
+			printf("Year of the tiger");
+			break;
+		}
+		case 3: {
+			printf("Year of the rabbit");
+			break;
+		}
+		case 4: {
+			printf("Year of the dragon");
+			break;
+		}
+		case 5: {
+			printf("Year of the snake");
+			break;
+		}
+		case 6: {
+			printf("Year of the horse");
+			break;
+		}
+		case 7: {
+			printf("Year of the sheep");
+			break;
+		}
+		case 8: {
+			printf("Year of the monkey");
+			break;
+		}
+		case 9: {
+			printf("Year of the rooster");
+			break;
+		}
+		case 10: {
+			printf("Year of the dog");
+			break;
+		}
+		case 11: {
+			printf("Year of the pig");
+			break;
+		}
+		default: {
+			printf("Year input error");
+			break;	
+		}
+	}	
 }
